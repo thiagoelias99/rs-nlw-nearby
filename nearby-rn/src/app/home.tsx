07c3e36918dbef2935753d2954fa5dfc8e2474b6
Thiago Elias <thiagoelias99@gmail.com>
2024-12-10 18:07:17 -0300

    feat: :sparkles: Fetch data in home

--- nearby-rn/src/app/home.tsx
+++ nearby-rn/src/app/home.tsx
@@ -1,7 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Alert } from 'react-native'
+
+import { api } from "@/services/api"
 
 export default function Home() {
+  const [categories, setCategories] = useState([])
+  const [category, setCategory] = useState("")
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      setCategories(data)
+      setCategory(data[0].id)
+    } catch (error) {
+      console.error(error)
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+    }
+  }
+
   return (
     <View>
       <Text>Home</Text>
