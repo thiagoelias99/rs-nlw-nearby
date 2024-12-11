9739bdcf15052a1120061d0400b4eb8cedad31b6
Thiago Elias <thiagoelias99@gmail.com>
2024-12-11 10:44:20 -0300

    feat: :sparkles: Maps

--- nearby-rn/src/app/home.tsx
+++ nearby-rn/src/app/home.tsx
@@ -5,6 +5,9 @@ import { api } from "@/services/api"
 import { Categories, CategoriesProps } from "@/components/categories"
 import { PlaceProps } from "@/components/place"
 import { Places } from "@/components/places"
+import MapView, { Callout, Marker } from "react-native-maps"
+import * as Location from "expo-location"
+import { colors, fontFamily } from "@/styles/theme"
 
 type MarketsProps = PlaceProps & {
   latitude: number
@@ -15,6 +18,7 @@ export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
   const [category, setCategory] = useState("")
   const [markets, setMarkets] = useState<MarketsProps[]>([])
+  const [currentLocation, setCurrentLocation] = useState({ latitude: -23.561187293883442, longitude: -46.656451388116494 })
 
   useEffect(() => {
     fetchCategories()
@@ -24,6 +28,10 @@ export default function Home() {
     if (!category) return
     fetchMarkets()
   }, [category])
+
+  // useEffect(() => {
+  //   getCurrentLocation()
+  // }, [])
 
   async function fetchCategories() {
     try {
@@ -50,6 +58,23 @@ export default function Home() {
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync()
+        console.log(location)
+        setCurrentLocation({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
       <Categories
@@ -57,6 +82,66 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
+
+      <MapView style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01, // zoom
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          title="Você está aqui"
+          description="Sua localização atual"
+          image={require("@/assets/location.png")} //Trocar ícone
+        />
+
+        {markets.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout
+            // onPress={() => router.navigate(`/market/${item.id}`)}
+            >
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+
+
+      </MapView>
 
       <Places data={markets} />
     </View>
